a177036f333e3c63baf9a24e22a387ed93d5670e
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-07-25 00:07:50 +0300

    Database Configuration

---
+++ config/db.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ 
+Now we want to connect to the Database
+There is two main ways (APIs) to connect to databases with Vanilla PHP: 
+    - MySQLi (MySQL Improved): 
+        * Works only with MySQL databases.
+        * Easier for beginners and supports both procedural and OOP styles.
+    - PDO (PHP Data Objects): 
+        * More flexible, can work with multiple database types.
+        * Preferred for advanced features.
+
+*/
+
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'Moha');
+define ('DB_PASS', '01001101');
+define ('DB_NAME', 'feedback_db');
+
+$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+} else {
+    echo "Connected successfully!";
+}
+
+
+?>
